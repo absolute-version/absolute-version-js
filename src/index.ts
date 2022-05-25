c0b4cf6038f8345da58260fd554f6d0f479b1699
Timothy Jones <timothy.l.jones@gmail.com>
2022-05-25 16:02:18 +1000

    feat: Add JSDoc for intellisense
    BREAKING CHANGE: There are no breaking changes, this release just marks the solidification of the public API.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { gitTagDescriptor } from './gitTagDescriptor';
 import { cleanForSemver, descriptorToString } from './output';
 import { AbsoluteVersionConfig } from './types';
 
+/** Reads the tag information from the git repository, and returns an absolute-version */
 export const versionFromGitTag = (config?: AbsoluteVersionConfig): string =>
   descriptorToString(gitTagDescriptor(cleanForSemver, applyDefaults(config)));
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+/** Complete configuration for AbsoluteVersion */
 export interface AbsoluteVersionConfig {
+  /** Glob for detecting version tags. The format for the glob is the same as for `git describe --match`.  Default is `v[0-9]*` */
   tagGlob: string;
 }
